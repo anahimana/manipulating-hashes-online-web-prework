4bb1029524253a0c3253d3bdfaf3767c7ea54736
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-02 19:22:13 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,15 +15,6 @@ def first_challenge
 
   #your code here
   contacts.each do |person, data|
-    if person == "Freddy Mercury"
-      data.each do |attribute, value|
-        if attribute == "favorite_icecream_flavors"
-          if attribute.include?("strawberry")
-            attribute.delete("strawberry")
-          end
-        end
-      end
-    end
   end
 
   #remember to return your newly altered contacts hash!
