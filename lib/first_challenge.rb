871c14358f2750b7b138f68ec0acd81c5b26b268
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-02 19:26:17 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,6 +17,7 @@ def first_challenge
   contacts.each do |person, data|
     if person == "Freddy Mercury"
       data.each do |attribute, value|
+        if attribute == :favorite_icecream_flavors
     end
   end
 
