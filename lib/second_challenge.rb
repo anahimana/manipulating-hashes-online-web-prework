c8df8d15af3421c4ab7d1f44e0bff9a473e55108
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-02 19:40:50 +0000

    Automatically backed up by Learn

---
+++ lib/second_challenge.rb
@@ -0,0 +1,16 @@
+def second_challenge
+  groceries = {
+   dairy: ["milk", "yogurt", "cheese"],
+   vegetable: ["carrots", "broccoli", "cucumbers"],
+   meat: ["chicken", "steak", "salmon"],
+   grains: ["rice", "pasta"]
+  }
+
+  #code your solution here!
+  groceries.collect do |item, value|
+    puts value
+  end
+
+end
+
+second_challenge
