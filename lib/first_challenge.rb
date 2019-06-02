3691eb18c6e82ed2da0ca84c6d9fbf0f5f5e79f1
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-02 19:07:20 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,6 +16,7 @@ def first_challenge
   #your code here
   contacts.each do |person, data|
     if person == "Freedy Mercury"
+    end
   end
 
   #remember to return your newly altered contacts hash!
