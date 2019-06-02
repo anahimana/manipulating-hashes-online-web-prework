7c65d7134b03b1d28d0f8c6f0fc401b75a8cd2d3
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-02 19:20:46 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -19,7 +19,7 @@ def first_challenge
       data.each do |attribute, value|
         if attribute == "favorite_icecream_flavors"
           if attribute.include?("strawberry")
-            
+            attribute.delete("strawberry")
           end
         end
       end
